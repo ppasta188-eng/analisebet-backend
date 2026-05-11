ed6a477463ff8c9eb40cdfbffea0ae810476d534
ppasta188-eng <ppasta188@gmail.com>
2026-05-11 15:04:34 -0300

    Update oddsService.js

--- services/oddsService.js
+++ services/oddsService.js
@@ -1,1 +1,25 @@
+import axios from "axios";
 
+const API_KEY = process.env.ODDS_API_KEY;
+
+export async function buscarOdds() {
+  try {
+    const response = await axios.get(
+      "https://api.the-odds-api.com/v4/sports/soccer_brazil_campeonato/odds",
+      {
+        params: {
+          apiKey: API_KEY,
+          regions: "eu",
+          markets: "h2h,totals,btts",
+          oddsFormat: "decimal",
+        },
+      }
+    );
+
+    return response.data;
+  } catch (error) {
+    console.log("Erro ao buscar odds:", error.message);
+
+    return [];
+  }
+}
