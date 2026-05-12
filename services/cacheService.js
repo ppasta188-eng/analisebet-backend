f7e301ea9314370377deb1c64e513961802ef8f6
ppasta188-eng <ppasta188@gmail.com>
2026-05-12 11:53:14 -0300

    Update cacheService.js

--- services/cacheService.js
+++ services/cacheService.js
@@ -8,6 +8,18 @@ export function obterJogos() {
   return jogosCache;
 }
 
+export function buscarJogosPorTime(nomeTime) {
+  return jogosCache.filter((jogo) => {
+    const home = jogo.home_team?.toLowerCase() || "";
+    const away = jogo.away_team?.toLowerCase() || "";
+
+    return (
+      home.includes(nomeTime.toLowerCase()) ||
+      away.includes(nomeTime.toLowerCase())
+    );
+  });
+}
+
 export function limparCache() {
   jogosCache = [];
 }
